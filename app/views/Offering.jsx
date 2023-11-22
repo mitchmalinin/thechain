881dd1cde5443f38fd219ab5485ad365174dfad7
Vlad B <vladimir.bazhutin@gmail.com>
2023-11-22 01:11:16 -0500

    Offering section refactor

--- app/views/Offering.jsx
+++ app/views/Offering.jsx
@@ -1,111 +1,62 @@
-'use client'
+"use client";
 
-import {
-  Box,
-  Image as ChakraImage,
-  Link as ChakraLink,
-  Flex,
-  SimpleGrid,
-  Text,
-} from '@chakra-ui/react'
-
-import { offerings } from '../utils/constants'
+import Link from "next/link";
+import { offerings } from "../utils/constants";
 
 export const Offering = () => {
-  return (
-    <Flex flexDir={'column'} alignItems={'center'} mt="6rem">
-      <Text
-        fontSize={{ lg: '24px', sm: '18px' }}
-        mb="1rem"
-        color="#ff62c7"
-        fontWeight="bold"
-      >
-        Benefits
-      </Text>
-      <SimpleGrid
-        id="offering"
-        columns={{ lg: 4, md: 2, sm: 1 }}
-        gap="5"
-        mt={{ lg: '1rem', sm: '0' }}
-        px={{ lg: '4rem', sm: '2rem' }}
-        maxW={'100rem'}
-      >
-        {offerings.map((offer, index) => {
-          return (
-            <Flex
-              key={index}
-              direction="column"
-              alignItems="center"
-              justifyContent="flex-start"
-              minH="150px"
-              p="1.5rem"
-              bg="#000000ed"
-              color={'white'}
-              borderRadius="0.5rem"
+    return (
+        <div className="flex flex-col items-center mt-24">
+            <h2 className="text-lg lg:text-2xl mb-8 text-[#ff62c7] font-bold">
+                Benefits
+            </h2>
+            <div
+                id="offering"
+                className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5 mt-4 lg:mt-0 px-8 sm:px-16 max-w-[100rem]"
             >
-              <ChakraImage
-                src={offer.icon}
-                alt="icon"
-                w="40px"
-                mb="15px"
-                filter="invert(1)"
-              />
-              <Text
-                fontSize={{ lg: '1rem', sm: '1rem' }}
-                fontWeight="bold"
-                mb="0x"
-                minH="45px"
-                textAlign="center"
-                color="#ff62c7"
-              >
-                {offer.title}
-              </Text>
-              <Text
-                color="white"
-                textAlign="center"
-                fontSize={{ sm: '14px', lg: '14px' }}
-                mt="5px"
-                opacity="0.8"
-              >
-                {offer.content}
-              </Text>
-              {index == 3 && (
-                <ChakraLink
-                  src="/#consult"
-                  mt="15px"
-                  fontSize={{ sm: '14px', lg: '14px' }}
-                  textDecoration="underline"
-                >
-                  Learn more
-                </ChakraLink>
-              )}
-            </Flex>
-          )
-        })}
-      </SimpleGrid>
-      <SimpleGrid
-        columns={{ sm: '1', lg: '3' }}
-        spacing={10}
-        mt={'64px'}
-        bg="#000000ed"
-        p={'2rem'}
-        color={'white'}
-        maxW={'100rem'}
-        gap={'4rem'}
-      >
-        {[
-          { metric: '400k+', description: 'social impressions' },
-          { metric: '1000+', description: 'connections made' },
-          { metric: '17+', description: 'past events' },
-        ].map((item, index) => (
-          <Box key={index} textAlign="center">
-            <Text fontSize="5xl" fontWeight="bold" color={'#42b5ff'}>
-              {item.metric}
-            </Text>
-            <Text fontSize="sm">{item.description}</Text>
-          </Box>
-        ))}
-      </SimpleGrid>
-    </Flex>
-  )
-}
+                {offerings.map((offer, index) => (
+                    <div
+                        key={index}
+                        className="flex flex-col items-center justify-start min-h-[150px] p-6 bg-[#000000ed] text-white rounded-lg"
+                    >
+                        <img
+                            src={offer.icon}
+                            alt="icon"
+                            className="w-10 mb-4 filter invert"
+                        />
+                        <h3 className="text-base font-bold mb-0 min-h-[45px] text-center text-[#ff62c7]">
+                            {offer.title}
+                        </h3>
+                        <p className="text-center text-sm mt-1 opacity-80">
+                            {offer.content}
+                        </p>
+                        {index === 3 && (
+                            <Link
+                                href="/#consult"
+                                className="mt-4 text-sm underline"
+                            >
+                                Learn more
+                            </Link>
+                        )}
+                    </div>
+                ))}
+            </div>
+            <div className="grid grid-cols-1 lg:grid-cols-3 gap-16 mt-16 bg-[#000000ed] px-8 p-10 text-white max-w-[100rem]">
+                {[
+                    { metric: "400k+", description: "social impressions" },
+                    { metric: "1000+", description: "connections made" },
+                    { metric: "17+", description: "past events" },
+                ].map((item, index) => (
+                    <div
+                        key={index}
+                        className="text-center gap-3 flex flex-col"
+                    >
+                        <p className="text-5xl font-bold text-[#42b5ff]">
+                            {item.metric}
+                        </p>
+                        <p className="text-sm">{item.description}</p>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+};
