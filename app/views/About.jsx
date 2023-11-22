99c35cd17e0643c4edf1678d3f6dca8a895f236b
Vlad B <vladimir.bazhutin@gmail.com>
2023-11-22 01:10:24 -0500

    About section refactor

--- app/views/About.jsx
+++ app/views/About.jsx
@@ -1,94 +1,65 @@
-'use client'
+"use client";
 
-import { SimpleGrid, Text, VStack } from '@chakra-ui/react'
-
-import { Tweet } from 'react-tweet'
+import { Tweet } from "react-tweet";
 
 export const About = () => {
-  return (
-    <>
-      <VStack
-        id="about"
-        alignItems={{ sm: 'flex-start', lg: 'center' }}
-        p={{ lg: '4rem', sm: '1rem' }}
-        mt="1rem"
-        maxW={'100rem'}
-        zIndex={100}
-      >
-        <Text
-          fontSize={{ lg: '24px', sm: '18px' }}
-          mb="1rem"
-          fontWeight="bold"
-          color="#ff62c7"
-        >
-          About Us
-        </Text>
-        <Text
-          w={{ lg: '70%', sm: '100%' }}
-          lineHeight="2"
-          fontSize={{ sm: '14px', lg: '16px' }}
-        >
-          Welcome to The Chain Miami, we are a curated community and social club
-          for the top web3 executives, builders, and enthusiasts in Miami.
-          Through bear markets and bull, we bring like-minded individuals
-          together, forging genuine friendships and expanding our IRL web3
-          networks.
-        </Text>
-        <Text
-          w={{ lg: '70%', sm: '100%' }}
-          lineHeight="2"
-          fontSize={{ sm: '14px', lg: '16px' }}
-        >
-          At The Chain Miami, we believe in the power of intentional community
-          building. We’re passed the days of generic happy hours and superficial
-          networking. We curate intentional experiences that leave you energized
-          and inspired. We’re here to foster the growth of Miami’s web3
-          community.
-        </Text>
-      </VStack>
-      <SimpleGrid
-        py="2rem"
-        gap="5"
-        columns={{ lg: '2', sm: '1' }}
-        placeItems="center"
-        px={{ lg: '4rem', sm: '2rem' }}
-        maxW={'100rem'}
-      >
-        <VStack alignItems="flex-start">
-          <Text
-            fontSize={{ lg: '24px', sm: '18px' }}
-            mb="1rem"
-            color="#ff62c7"
-            fontWeight="bold"
-          >
-            The Chain's Origin Story
-          </Text>
-          <Text opacity="0.8" fontSize={{ sm: '14px', lg: '16px' }}>
-            In the midst of the bull run of 2022, Monica discovered a remarkable
-            truth about Miami – a city with people passionate about web3, yet no
-            place for them to connect.
-          </Text>
-          <Text opacity="0.8" fontSize={{ sm: '14px', lg: '16px' }}>
-            With a simple tweet to gauge the presence of the local web3
-            community, Monica's expectations were surpassed revealing an
-            undeniable need for connection and IRL community for web3 builders
-            in Miami.
-          </Text>
-          <Text opacity="0.8" fontSize={{ sm: '14px', lg: '16px' }}>
-            In June 2022, The Chain was born, a community for local builders and
-            forward-thinkers in the web3 space to connect through monthly
-            dinners and keep the local web3 community up-to-date by launching a
-            texting community that shares upcoming events, and a celebration of
-            community wins.
-          </Text>
-          <Text opacity="0.8" fontSize={{ sm: '14px', lg: '16px' }}>
-            In the past year, we've hosted 12 dinners & experiences, uniting
-            500+ people. Now, as we enter our growth stage, we're more
-            intentional than ever in curating our community.
-          </Text>
-        </VStack>
-        <Tweet id="1524019928273199111" />
-      </SimpleGrid>
-    </>
-  )
-}
+    return (
+        <>
+            <div
+                id="about"
+                className="flex flex-col items-start lg:items-center p-4 lg:p-16 mt-4 max-w-[100rem] z-10"
+            >
+                <h2 className="text-lg lg:text-2xl mb-4 font-bold text-[#ff62c7]">
+                    About Us
+                </h2>
+                <p className="lg:w-2/3 w-full text-base mb-2 leading-8">
+                    Welcome to The Chain Miami, we are a curated community and
+                    social club for the top web3 executives, builders, and
+                    enthusiasts in Miami. Through bear markets and bull, we
+                    bring like-minded individuals together, forging genuine
+                    friendships and expanding our IRL web3 networks.
+                </p>
+                <p className="lg:w-2/3 w-full text-base leading-8">
+                    At The Chain Miami, we believe in the power of intentional
+                    community building. We’re passed the days of generic happy
+                    hours and superficial networking. We curate intentional
+                    experiences that leave you energized and inspired. We’re
+                    here to foster the growth of Miami’s web3 community.
+                </p>
+            </div>
+            <div className="py-8 grid gap-5 lg:grid-cols-2 grid-cols-1 place-items-center px-16 max-w-[100rem]">
+                <div className="flex flex-col items-start gap-2">
+                    <h2 className="text-lg lg:text-2xl mb-4 text-[#ff62c7] font-bold">
+                        The Chain's Origin Story
+                    </h2>
+                    <p className="opacity-80 text-base">
+                        In the midst of the bull run of 2022, Monica discovered
+                        a remarkable truth about Miami – a city with people
+                        passionate about web3, yet no place for them to connect.
+                    </p>
+                    <p className="opacity-80 text-base">
+                        With a simple tweet to gauge the presence of the local
+                        web3 community, Monica's expectations were surpassed
+                        revealing an undeniable need for connection and IRL
+                        community for web3 builders in Miami.
+                    </p>
+                    <p className="opacity-80 text-base">
+                        In June 2022, The Chain was born, a community for local
+                        builders and forward-thinkers in the web3 space to
+                        connect through monthly dinners and keep the local web3
+                        community up-to-date by launching a texting community
+                        that shares upcoming events, and a celebration of
+                        community wins.
+                    </p>
+                    <p className="opacity-80 text-base">
+                        In the past year, we've hosted 12 dinners & experiences,
+                        uniting 500+ people. Now, as we enter our growth stage,
+                        we're more intentional than ever in curating our
+                        community.
+                    </p>
+                </div>
+                <Tweet id="1524019928273199111" />
+            </div>
+        </>
+    );
+};
