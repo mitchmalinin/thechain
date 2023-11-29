90812577a37a67b73764e294cf56d730446ac143
Mitch Malinin <mitchmalinin@gmail.com>
2023-11-29 16:57:15 -0500

    testing

--- app/views/About.jsx
+++ app/views/About.jsx
@@ -3,23 +3,25 @@
 import { Tweet } from "react-tweet";
 
 export const About = () => {
+    console.log("test");
+    const test;
     return (
         <>
             <div
                 id="about"
-                className="flex flex-col items-start lg:items-center p-4 lg:p-16 mt-4 max-w-[100rem] z-10"
+                className="z-10 mt-4 flex max-w-[100rem] flex-col items-start p-4 lg:items-center lg:p-16"
             >
-                <h2 className="text-lg lg:text-2xl mb-4 font-bold text-[#ff62c7]">
+                <h2 className="mb-4 text-lg font-bold text-[#ff62c7] lg:text-2xl">
                     About Us
                 </h2>
-                <p className="lg:w-2/3 w-full text-base mb-2 leading-8">
+                <p className="mb-2 w-full text-base leading-8 lg:w-2/3">
                     Welcome to The Chain Miami, we are a curated community and
                     social club for the top web3 executives, builders, and
                     enthusiasts in Miami. Through bear markets and bull, we
                     bring like-minded individuals together, forging genuine
                     friendships and expanding our IRL web3 networks.
                 </p>
-                <p className="lg:w-2/3 w-full text-base leading-8">
+                <p className="w-full text-base leading-8 lg:w-2/3">
                     At The Chain Miami, we believe in the power of intentional
                     community building. We’re passed the days of generic happy
                     hours and superficial networking. We curate intentional
@@ -27,23 +29,23 @@ export const About = () => {
                     here to foster the growth of Miami’s web3 community.
                 </p>
             </div>
-            <div className="py-8 grid gap-5 lg:grid-cols-2 grid-cols-1 place-items-center px-16 max-w-[100rem]">
+            <div className="grid max-w-[100rem] grid-cols-1 place-items-center gap-5 px-16 py-8 lg:grid-cols-2">
                 <div className="flex flex-col items-start gap-2">
-                    <h2 className="text-lg lg:text-2xl mb-4 text-[#ff62c7] font-bold">
+                    <h2 className="mb-4 text-lg font-bold text-[#ff62c7] lg:text-2xl">
                         The Chain's Origin Story
                     </h2>
-                    <p className="opacity-80 text-base">
+                    <p className="text-base opacity-80">
                         In the midst of the bull run of 2022, Monica discovered
                         a remarkable truth about Miami – a city with people
                         passionate about web3, yet no place for them to connect.
                     </p>
-                    <p className="opacity-80 text-base">
+                    <p className="text-base opacity-80">
                         With a simple tweet to gauge the presence of the local
                         web3 community, Monica's expectations were surpassed
                         revealing an undeniable need for connection and IRL
                         community for web3 builders in Miami.
                     </p>
-                    <p className="opacity-80 text-base">
+                    <p className="text-base opacity-80">
                         In June 2022, The Chain was born, a community for local
                         builders and forward-thinkers in the web3 space to
                         connect through monthly dinners and keep the local web3
@@ -51,7 +53,7 @@ export const About = () => {
                         that shares upcoming events, and a celebration of
                         community wins.
                     </p>
-                    <p className="opacity-80 text-base">
+                    <p className="text-base opacity-80">
                         In the past year, we've hosted 12 dinners & experiences,
                         uniting 500+ people. Now, as we enter our growth stage,
                         we're more intentional than ever in curating our
