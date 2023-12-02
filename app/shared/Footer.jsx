d6fc27fbfb5a54d423ad0297a61d24d13ac7d0fc
Mitch Malinin <mitchmalinin@gmail.com>
2023-12-01 22:44:40 -0500

    testing

--- app/shared/Footer.jsx
+++ app/shared/Footer.jsx
@@ -5,12 +5,12 @@ export const Footer = () => {
     return (
         <footer className="flex flex-col">
             <div id="footer-background" />
-            <div className="w-full flex flex-col-reverse md:flex-row items-start justify-between px-8 py-4 lg:px-20 lg:py-8 bg-black">
+            <div className="flex w-full flex-col-reverse items-start justify-between bg-black px-8 py-4 md:flex-row lg:px-20 lg:py-8">
                 <div className="flex flex-col items-start gap-6">
-                    <h2 className="font-bold lg:mb-3 text-lg lg:text-2xl lg:mt-0 sm:mt-6 text-[#ff62c7]">
+                    <h2 className="text-lg font-bold text-[#ff62c7] sm:mt-6 lg:mb-3 lg:mt-0 lg:text-2xl">
                         The Chain
                     </h2>
-                    <div className="flex text-white text-2xl gap-2">
+                    <div className="flex gap-2 text-2xl text-white">
                         <Link
                             href="https://twitter.com/thechain_miami"
                             target="_blank"
@@ -28,33 +28,33 @@ export const Footer = () => {
                     </div>
                 </div>
 
-                <div className="grid grid-cols-2 gap-8 lg:gap-20 mt-8 md:mt-0">
+                <div className="mt-8 grid grid-cols-2 gap-8 md:mt-0 lg:gap-20">
                     <div className="flex flex-col items-start gap-2">
-                        <h3 className="text-base lg:text-lg text-blue-400 font-bold">
+                        <h3 className="text-base font-bold text-blue-400 lg:text-lg">
                             For Community
                         </h3>
                         <Link
                             href="/#apply"
-                            className="text-sm lg:text-base text-white cursor-pointer opacity-80"
+                            className="cursor-pointer text-sm text-white opacity-80 lg:text-base"
                         >
                             Join Us
                         </Link>
                     </div>
                     <div className="flex flex-col items-start gap-2">
-                        <h3 className="text-base lg:text-lg text-blue-400 font-bold">
+                        <h3 className="text-base font-bold text-blue-400 lg:text-lg">
                             For Clients
                         </h3>
                         <Link
                             href="/#consult"
-                            className="text-sm lg:text-base text-white cursor-pointer opacity-80"
+                            className="cursor-pointer text-sm text-white opacity-80 lg:text-base"
                         >
                             Hire Us
                         </Link>
                     </div>
                 </div>
             </div>
-            <div className="bg-black text-white px-8 lg:px-20">
-                <p className="text-xs my-2.5 text-center">
+            <div className="bg-black px-8 text-white lg:px-20">
+                <p className="my-2.5 text-center text-xs">
                     the chain miami © 2023
                 </p>
             </div>
