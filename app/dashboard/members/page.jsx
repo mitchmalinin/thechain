9cf4cb8420363863eaef03082ef9503885f05ddb
Mitch Malinin <mitchmalinin@gmail.com>
2023-12-12 20:02:17 -0500

    added search

--- app/dashboard/members/page.jsx
+++ app/dashboard/members/page.jsx
@@ -1,86 +1,116 @@
 "use client";
+import { useDebounce } from "@/app/hooks/useDebounce";
 import Link from "next/link";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { AiFillLinkedin, AiFillTwitterSquare } from "react-icons/ai";
 import useSWR from "swr";
 
 export default function Members() {
     const fetcher = (url) => fetch(url).then((r) => r.json());
+    const [searchTerm, setSearchTerm] = useState("");
+    const [filter, setFilter] = useState("");
+
+    const debouncedSearchTerm = useDebounce(searchTerm, 500);
 
     const {
         data: members,
         error,
         isLoading,
-    } = useSWR("/api/community", fetcher);
+    } = useSWR(
+        `/api/community?search=${debouncedSearchTerm}&filter=${filter}`,
+        fetcher,
+    );
 
     useEffect(() => {
-        console.log(members);
-    }, [members]);
+        if (debouncedSearchTerm) {
+            setSearchTerm(debouncedSearchTerm);
+        }
+    }, [debouncedSearchTerm]);
 
     if (isLoading)
         return (
-            <div className="min-h-screen flex items-center justify-center">
-                <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-purple-500"></div>
+            <div className="flex min-h-screen items-center justify-center">
+                <div className="h-32 w-32 animate-spin rounded-full border-b-2 border-t-2 border-purple-500"></div>
             </div>
         );
 
     if (!members)
         return (
-            <div className="min-h-screen flex items-center justify-center">
+            <div className="flex min-h-screen items-center justify-center">
                 <div>Must be Chain Member to view</div>
             </div>
         );
 
     if (error)
         return (
-            <div className="min-h-screen flex items-center justify-center">
+            <div className="flex min-h-screen items-center justify-center">
                 <div>Failed to load members</div>
             </div>
         );
 
     return (
-        <div className="grid grid-cols-1 md:grid-cols-4 gap-10 p-8 min-h-screen">
-            {members.map((member) => (
-                <div
-                    key={member.ID}
-                    className="p-5 shadow-md border rounded-lg space-y-3 flex flex-col justify-between"
+        <div className="grid min-h-screen content-start px-8 pt-4">
+            <div className="flex gap-4 pb-4">
+                <input
+                    type="text"
+                    placeholder="Search..."
+                    onChange={(e) => setSearchTerm(e.target.value)}
+                    value={searchTerm}
+                    className="rounded border-2 border-gray-300 px-4"
+                />
+                <select
+                    onChange={(e) => setFilter(e.target.value)}
+                    className="rounded bg-gray-300 p-2"
                 >
-                    <div className="w-full overflow-hidden bg-gray-200 flex items-center justify-center h-40">
-                        {/* Placeholder for member image */}
-                        <span className="text-gray-500 text-2xl">Photo</span>
-                    </div>
-                    <div className="flex flex-col gap-4 flex-grow">
-                        <div>
-                            <h3 className="text-lg font-black">
-                                {member.name}
-                            </h3>
-                            <p className="text-base">{member.occupation}</p>
+                    <option value="">All</option>
+                    <option value="accepted">Is member</option>
+                </select>
+            </div>
+            <div className="grid grid-cols-1 gap-10 md:grid-cols-4">
+                {members.map((member) => (
+                    <div
+                        key={member.ID}
+                        className="flex flex-col justify-between space-y-3 rounded-lg border p-5 shadow-md"
+                    >
+                        <div className="flex h-40 w-full items-center justify-center overflow-hidden bg-gray-200">
+                            {/* Placeholder for member image */}
+                            <span className="text-2xl text-gray-500">
+                                Photo
+                            </span>
                         </div>
-                        <p className="text-sm">{member.contribution}</p>
-                    </div>
+                        <div className="flex flex-grow flex-col gap-4">
+                            <div>
+                                <h3 className="text-lg font-black">
+                                    {member.name}
+                                </h3>
+                                <p className="text-base">{member.occupation}</p>
+                            </div>
+                            <p className="text-sm">{member.contribution}</p>
+                        </div>
 
-                    <div className="flex flex-row justify-center text-4xl">
-                        {member.twitter && (
-                            <Link
-                                href={`https://twitter.com/${member.twitter}`}
-                                target="_blank"
-                                className="text-blue-600 hover:underline"
-                            >
-                                <AiFillTwitterSquare />
-                            </Link>
-                        )}
-                        {member.linkedin && (
-                            <Link
-                                href={member.linkedin}
-                                target="_blank"
-                                className="text-blue-600 hover:underline"
-                            >
-                                <AiFillLinkedin />
-                            </Link>
-                        )}
+                        <div className="flex flex-row justify-center text-4xl">
+                            {member.twitter && (
+                                <Link
+                                    href={`https://twitter.com/${member.twitter}`}
+                                    target="_blank"
+                                    className="text-blue-600 hover:underline"
+                                >
+                                    <AiFillTwitterSquare />
+                                </Link>
+                            )}
+                            {member.linkedin && (
+                                <Link
+                                    href={member.linkedin}
+                                    target="_blank"
+                                    className="text-blue-600 hover:underline"
+                                >
+                                    <AiFillLinkedin />
+                                </Link>
+                            )}
+                        </div>
                     </div>
-                </div>
-            ))}
+                ))}
+            </div>
         </div>
     );
 }
